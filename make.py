c55e4299481c855c84c9fd26da192cf0a2a7467d
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-10-08 13:14:19 -0700

    Update make.py

--- make.py
+++ make.py
@@ -1,8 +1,14 @@
 
-
+import pygccxml
 def makeMinorGems():
     pass
 def makeOneLife():
     pass
 def make(folder):
+    pass
+def parseCpp(file):
+    pass
+def generatePYX(data):
+    pass
+def generatePXD(data):
     pass
