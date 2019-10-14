025225699a8cb02fcbad1166d0d9abeb7adcbd5f
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-10-14 07:57:24 -0700

    Create declPYX.py

---
+++ declPYX.py
@@ -0,0 +1,11 @@
+# contains classes that represent decls in cpp/h files
+class Base():
+    pass
+class func(Base): # function
+    pass    
+class cppclass(Base):
+    pass
+class include(Base):
+    pass
+class prop(Base): # property
+    pass
