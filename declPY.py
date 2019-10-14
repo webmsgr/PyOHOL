d766a7d30c38fbb6df05d476d587b80ae1c9f082
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-10-14 08:03:24 -0700

    Update declPY.py

--- declPY.py
+++ declPY.py
@@ -1,6 +1,18 @@
 # contains classes that represent decls in cpp/h files
 class Base():
-    pass
+    def __init__(self,data,indent):
+        self.data = data
+        self.functions = []
+        self.classes = []
+        self.props = []
+        self.indent = indent*4
+        self.indentlevel = indent
+    def parse(self):
+        pass # takes self.data and populates all other properties
+    def toPYX(self):
+        return ""
+    def toPXD(self):
+        return ""
 class func(Base): # function
     pass    
 class cppclass(Base):
