ad0bc40eb584ec5c86842510c9aa74c1947d033c
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-10-14 11:10:30 -0700

    Delete test_decls.py

--- test_decls.py
+++
@@ -1,9 +0,0 @@
-import declPY
-import make
-
-def get_data():
-    data = declPY.namespace(make.parseCpp("./OneLife/server/map.h"),0)
-    return data
-def test_type():
-    data = get_data()
-    assert isinstance(data,declPY.namespace)
