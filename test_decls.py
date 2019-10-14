58e4ba04633e9fd08c95c16a1c32cc6bd5161a30
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-10-14 11:09:46 -0700

    Update test_decls.py

--- test_decls.py
+++ test_decls.py
@@ -4,3 +4,6 @@ import make
 def get_data():
     data = declPY.namespace(make.parseCpp("./OneLife/server/map.h"),0)
     return data
+def test_type():
+    data = get_data()
+    assert isinstance(data,declPY.namespace)
