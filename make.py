9ffcce0a952290a53081adcae42b4eff55af82ea
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-10-17 09:44:39 -0700

    Update make.py

--- make.py
+++ make.py
@@ -6,6 +6,7 @@ from pygccxml import declarations
 from pygccxml import parser
 import os
 import py_decl
+import glob
 # Find the location of the xml generator (castxml or gccxml)
 generator_path, generator_name = utils.find_xml_generator()
 
@@ -16,10 +17,21 @@ xml_generator_config = parser.xml_generator_configuration_t(xml_generator_path=g
 def makeMinorGems():
     pass
 def makeOneLife():
-    pass
-def make(folder):
+    incfiles = []
+    for file in glob.glob("OneLife/**/*.h"):
+        incfiles += py_decl.convertns(parse_file(file))
+    files = {}
+    for file in incfiles:
+        if file.fl in files:
+            files[file.fl].things += file.things
+        else:
+            files[file.fl] = file
+    print("Found {} files to convert".format(len(files)))
+def make(file):
     pass
 def parseCpp(file):
+    return parse_file(file)
+def parse_file(file):
     # Parse the c++ file
     decls = parser.parse([file], xml_generator_config)
 
