57684d4256e0254d57f74fc60028e72e826e1535
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-10-17 13:12:35 -0700

    Update setup_template.py

--- setup_template.py
+++ setup_template.py
@@ -15,7 +15,7 @@ ext = '.pyx' if USE_CYTHON else '.cpp'
 exts = []
 
 for file in glob.glob("OneLife/**/py_*"+ext) + glob.glob("minorGems/**/py_*"+ext):
-  exts.append(Extension(file.replace("/",".").replace(ext,""),[file],language='c++',include_dirs=[]))
+    exts.append(Extension(file.replace("/",".").replace(ext,""),[file],language='c++',include_dirs=[]))
 
 
 if USE_CYTHON:
