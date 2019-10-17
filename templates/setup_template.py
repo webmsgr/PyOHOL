612dadd2434dc0bf777b73d8dd35fb1913b8faae
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-10-17 08:03:02 -0700

    Update setup_template.py

--- templates/setup_template.py
+++ templates/setup_template.py
@@ -1,2 +1,27 @@
 # insert setup.py here
 # todo implement
+
+from distutils.core import setup
+from distutils.extension import Extension
+import sys
+import glob
+
+if '--use-cython' in sys.argv:
+    USE_CYTHON = True
+    sys.argv.remove('--use-cython')
+else:
+    USE_CYTHON = False
+ext = '.pyx' if USE_CYTHON else '.cpp'
+extensions = []
+
+for file in glob.glob("OneLife/**/py_*"+ext):
+  extentions.append(Extension(file.replace("/",".").replace(ext,""),[file],language='c++',include_dirs=[]))
+
+
+if USE_CYTHON:
+    from Cython.Build import cythonize
+    extensions = cythonize(extensions)
+
+setup(
+    ext_modules = extensions
+)
